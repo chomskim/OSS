16e7a5369399504a7e6f7eb6372c09e1209d31c2
chomskim <csknova@gmail.com>
2021-03-27 10:53:16 +0900

    commit  intro-0 intro-1

---
+++ intro-pgzero/intro-0.py
@@ -0,0 +1,9 @@
+import pgzrun
+
+WIDTH = 300
+HEIGHT = 300
+
+def draw():
+    screen.fill((128, 0, 0))
+
+pgzrun.go()	
--- intro-pgzero/intro-1.py
+++ intro-pgzero/intro-1.py
@@ -1,9 +1,8 @@
 import pgzrun
-import time
 
 alien = Actor('alien')
 #alien.pos = 100, 56
-alien.topright = 0, 10
+alien.topright = 0, 10 #(x,y)
 
 WIDTH = 500
 HEIGHT = alien.height + 20
@@ -19,17 +18,9 @@ def update():
 
 def on_mouse_down(pos):
     if alien.collidepoint(pos):
-        set_alien_hurt()
-
-
-def set_alien_hurt():
-    alien.image = 'alien_hurt'
-    sounds.eep.play()
-    clock.schedule_unique(set_alien_normal, 1.0)
-
-
-def set_alien_normal():
-    alien.image = 'alien'
-
+        sounds.eep.play()
+        alien.image = 'alien_hurt'
+    else:
+        print("You missed me!")
 
 pgzrun.go()	
